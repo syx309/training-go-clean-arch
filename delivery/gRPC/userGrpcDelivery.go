48ccaf8cf67eeaddf4fcaa9da1a60ef856c4d7ee
syx309 <alfredsoegiarto@gmail.com>
2021-02-07 18:53:12 +0700

    finishing gRPC server

--- delivery/gRPC/userGrpcDelivery.go
+++ delivery/gRPC/userGrpcDelivery.go
@@ -1,14 +1,18 @@
 package delivery
 
 import (
+	"fmt"
 	"gitlab.com/alfred_soegiarto/training-clean-arch/domain"
 	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"log"
+	"net"
 	"strconv"
 )
 
-type Server struct {}
 type userGRPCDelivery struct {
 	userUsecase domain.UserUsecase
+	UnimplementedUserServiceServer
 }
 
 func NewUserGRPCDelivery(uc domain.UserUsecase) *userGRPCDelivery{
@@ -40,4 +44,44 @@ func (u *userGRPCDelivery) FetchAll(ctx context.Context, _ *FetchAllRequest) (*F
 		Users: results,
 		Error: "No errors",
 	}, err
+}
+
+func (u *userGRPCDelivery) GetById(ctx context.Context, request *UserRequest) (*User, error) {
+	panic("implement me")
+}
+
+func (u *userGRPCDelivery) GetUserItem(ctx context.Context, request *EmailRequest) (*Item, error) {
+	panic("implement me")
+}
+
+func (u *userGRPCDelivery) Update(ctx context.Context, request *UpdateRequest) (*GeneralReply, error) {
+	panic("implement me")
+}
+
+func (u *userGRPCDelivery) Insert(ctx context.Context, request *InsertRequest) (*GeneralReply, error) {
+	panic("implement me")
+}
+
+func (u *userGRPCDelivery) Delete(ctx context.Context, request *DeleteRequest) (*GeneralReply, error) {
+	panic("implement me")
+}
+
+// Serve the gRPC service
+func (u *userGRPCDelivery) Serve() error {
+	lis, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		fmt.Println("delivery: ")
+		log.Fatal(err.Error())
+		return err
+	}
+
+	grpcServer := grpc.NewServer()
+	RegisterUserServiceServer(grpcServer, u)
+
+	if err := grpcServer.Serve(lis); err != nil {
+		fmt.Println("delivery: ")
+		log.Fatal(err.Error())
+		return err
+	}
+	return nil
 }
